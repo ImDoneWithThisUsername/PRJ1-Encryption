5d48915a1fac83d7fdf8099cf01cd0ca9536b00e
Minh <94094632+ImDoneWithThisUsername@users.noreply.github.com>
2022-10-15 18:42:12 +0700

    update encrypt and decrypt privatekey using aes

--- accounts/encryption.py
+++ accounts/encryption.py
@@ -1,9 +1,23 @@
 from Crypto.PublicKey import RSA
 from Crypto.Random import get_random_bytes
 from Crypto.Cipher import AES, PKCS1_OAEP
+import hashlib
 
+#RSA generator
 key = RSA.generate(2048)
 private_key = key.export_key()
+
+#Encrypt RSA private key 
+#Using AES, passphase = user input
+#@TODO
+aes_privatekey = AES.new(hashlib.sha256("test passphase".encode()).digest(), AES.MODE_EAX)
+ciphertext, tag = aes_privatekey.encrypt_and_digest(private_key)
+cipher_aes = AES.new(hashlib.sha256("test passphase".encode()).digest(), AES.MODE_EAX, aes_privatekey.nonce)
+data = cipher_aes.decrypt_and_verify(ciphertext, tag)
+print(data == private_key)
+
+
+#write to file
 file_out = open("private.pem", "wb")
 file_out.write(private_key)
 file_out.close()
@@ -18,6 +32,8 @@ file_out = open("encrypted_data.bin", "wb")
 
 recipient_key = RSA.import_key(open("receiver.pem").read())
 session_key = get_random_bytes(16)
+
+
 
 # Encrypt the session key with the public RSA key
 cipher_rsa = PKCS1_OAEP.new(recipient_key)
