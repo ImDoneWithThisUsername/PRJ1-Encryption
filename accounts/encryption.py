bc4c7b6ddc6100c254cc424f343cdbc50162793e
Minh <94094632+ImDoneWithThisUsername@users.noreply.github.com>
2022-10-16 08:44:57 +0700

    store private key to file

--- accounts/encryption.py
+++ accounts/encryption.py
@@ -3,61 +3,115 @@ from Crypto.Random import get_random_bytes
 from Crypto.Cipher import AES, PKCS1_OAEP
 import hashlib
 
-#RSA generator
-key = RSA.generate(2048)
-private_key = key.export_key()
+def generate_rsa_key():
+   key = RSA.generate(2048)
+   return key
 
-#Encrypt RSA private key 
-#Using AES, passphase = user input
-#@TODO
-aes_privatekey = AES.new(hashlib.sha256("test passphase".encode()).digest(), AES.MODE_EAX)
-ciphertext, tag = aes_privatekey.encrypt_and_digest(private_key)
-cipher_aes = AES.new(hashlib.sha256("test passphase".encode()).digest(), AES.MODE_EAX, aes_privatekey.nonce)
-data = cipher_aes.decrypt_and_verify(ciphertext, tag)
-print(data == private_key)
+def encrypt_rsa_private_key(passphrase: str, rsa_key: RSA.RsaKey) -> tuple[bytes, bytes, bytes]:
+   private_key = rsa_key.export_key()
+   #Encrypt RSA private key
+   #Using AES, passphrase = user input
+   ## Get aes private key using hashed passphrase for encryption 
+   aes_privatekey = AES.new(hashlib.sha256(passphrase.encode()).digest(), AES.MODE_EAX)
+   ## Encrypt RSA private key
+   cipherkey, tag = aes_privatekey.encrypt_and_digest(private_key)
+   return cipherkey, tag, aes_privatekey.nonce
 
+def write_encrypted_private_key_to_file(cipherkey: bytes, tag: bytes, nonce: bytes, file_path: str):
+   file_out = open(file_path, "wb")
+   [ file_out.write(x) for x in (nonce, tag, cipherkey) ]
+   file_out.close()
+   return file_path
 
-#write to file
-file_out = open("private.pem", "wb")
-file_out.write(private_key)
-file_out.close()
+def read_encrypted_private_key_from_file(file_path: str):
+   file_in = open(file_path, "rb")
+   nonce, tag, cipherkey = \
+      [ file_in.read(x) for x in (16, 16, -1) ]
+   file_in.close()
+   return cipherkey, tag, nonce
 
-public_key = key.publickey().export_key()
-file_out = open("receiver.pem", "wb")
-file_out.write(public_key)
-file_out.close()
+def decrypt_rsa_private_key(passphrase: str, cipherkey: bytes, tag: bytes, nonce:bytes) -> RSA.RsaKey:
+   aes_privatekey = AES.new(hashlib.sha256(passphrase.encode()).digest(), AES.MODE_EAX, nonce)
+   private_key = aes_privatekey.decrypt_and_verify(cipherkey, tag)
+   return RSA.import_key(private_key)
 
-data = "I met aliens in UFO. Here is the map.".encode("utf-8")
-file_out = open("encrypted_data.bin", "wb")
+def encrypt_file(file_path):
+   pass
 
-recipient_key = RSA.import_key(open("receiver.pem").read())
-session_key = get_random_bytes(16)
+def decrypt_file(file_path):
+   pass
 
+if __name__ == '__main__':
+   debug = True
+   if debug == False:
+      exit()
+   #RSA generator
+   key = RSA.generate(2048)
+   # print(type(key))
+   private_key = key.export_key()
 
+   #Encrypt RSA private key 
+   #Using AES, passphrase = user input
+   ## Get aes private key using hashed passphrase for encryption 
+   aes_privatekey = AES.new(hashlib.sha256("test passphrase".encode()).digest(), AES.MODE_EAX)
+   ## Encrypt RSA private key 
+   ciphertext, tag = aes_privatekey.encrypt_and_digest(private_key)
+   print(type(ciphertext))
+   print(type(tag))
+   print(type(aes_privatekey.nonce))
+   ## Get aes private key using hashed passphrase for decryption
+   cipher_aes = AES.new(hashlib.sha256("test passphrase".encode()).digest(), AES.MODE_EAX, aes_privatekey.nonce)
+   ## Decrypt rsa private key
+   data = cipher_aes.decrypt_and_verify(ciphertext, tag)
+   print(data == private_key)
+   #write rsa private key to file
+   file_out = open("private.pem", "wb")
+   file_out.write(private_key)
+   file_out.close()
 
-# Encrypt the session key with the public RSA key
-cipher_rsa = PKCS1_OAEP.new(recipient_key)
-enc_session_key = cipher_rsa.encrypt(session_key)
+   #write rsa public key to file
+   public_key = key.publickey().export_key()
+   file_out = open("receiver.pem", "wb")
+   file_out.write(public_key)
+   file_out.close()
 
-# Encrypt the data with the AES session key
-cipher_aes = AES.new(session_key, AES.MODE_EAX)
-ciphertext, tag = cipher_aes.encrypt_and_digest(data)
-[ file_out.write(x) for x in (enc_session_key, cipher_aes.nonce, tag, ciphertext) ]
-file_out.close()
+   data = "I met aliens in UFO. Here is the map.".encode("utf-8")
+   file_out = open("encrypted_data.bin", "wb")
 
-######################################################################################
-file_in = open("encrypted_data.bin", "rb")
+   #read public key from file
+   recipient_key = RSA.import_key(open("receiver.pem").read())
+   #generate aes session key
+   session_key = get_random_bytes(16)
 
-private_key = RSA.import_key(open("private.pem").read())
+   # Encrypt session key with the public RSA key
+   cipher_rsa = PKCS1_OAEP.new(recipient_key)
+   enc_session_key = cipher_rsa.encrypt(session_key)
 
-enc_session_key, nonce, tag, ciphertext = \
-   [ file_in.read(x) for x in (private_key.size_in_bytes(), 16, 16, -1) ]
+   # Encrypt data with the AES session key
+   # get key
+   cipher_aes = AES.new(session_key, AES.MODE_EAX)
+   # encrypt
+   ciphertext, tag = cipher_aes.encrypt_and_digest(data)
+   # write to file
+   [ file_out.write(x) for x in (enc_session_key, cipher_aes.nonce, tag, ciphertext) ]
+   file_out.close()
 
-# Decrypt the session key with the private RSA key
-cipher_rsa = PKCS1_OAEP.new(private_key)
-session_key = cipher_rsa.decrypt(enc_session_key)
+   ######################################################################################
+   # open cipher text
+   file_in = open("encrypted_data.bin", "rb")
 
-# Decrypt the data with the AES session key
-cipher_aes = AES.new(session_key, AES.MODE_EAX, nonce)
-data = cipher_aes.decrypt_and_verify(ciphertext, tag)
-print(data.decode("utf-8"))
+   # read private key from file
+   private_key = RSA.import_key(open("private.pem").read())
+
+   # read from file
+   enc_session_key, nonce, tag, ciphertext = \
+      [ file_in.read(x) for x in (private_key.size_in_bytes(), 16, 16, -1) ]
+
+   # Decrypt the session key with the private RSA key
+   cipher_rsa = PKCS1_OAEP.new(private_key)
+   session_key = cipher_rsa.decrypt(enc_session_key)
+
+   # Decrypt the data with the AES session key
+   cipher_aes = AES.new(session_key, AES.MODE_EAX, nonce)
+   data = cipher_aes.decrypt_and_verify(ciphertext, tag)
+   print(data.decode("utf-8"))
--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from . import encryption
 
 # Create your tests here.
+class EncryptionTest(TestCase):
+    def test_encryption_decryption_private_key(self):
+        key = encryption.generate_rsa_key()
+        cipherkey, tag, nonce = encryption.encrypt_rsa_private_key("test",key)
+        file_path = encryption.write_encrypted_private_key_to_file(cipherkey, tag, nonce, "key.bin")
+        cipherkey, tag, nonce = encryption.read_encrypted_private_key_from_file(file_path)
+        key_decrypt = encryption.decrypt_rsa_private_key("test", cipherkey, tag, nonce)
+        self.assertEqual(key, key_decrypt)
