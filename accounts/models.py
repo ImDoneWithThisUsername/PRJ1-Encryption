1c6717f55c4249c52492e377af43568db897565b
Minh <94094632+ImDoneWithThisUsername@users.noreply.github.com>
2022-10-16 20:01:16 +0700

    nth

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.utils.translation import gettext_lazy as _
-
 class CustomUserManager(BaseUserManager):
     def create_user(self, email, password, **extra_fields):
         if not email:
